2818b22ad73f2e3ba07376bddfc2b172d7116492
Benjamin-Manjolo <benjaminmanjolo39@gmail.com>
2026-06-04 15:55:07 +0200

    fixed hero section

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -3,7 +3,7 @@ import { HeroContent } from "@/sections/Hero/components/HeroContent";
 
 export const Hero = () => {
   return (
-    <main className="relative content-center items-center box-border caret-transparent gap-x-8 flex flex-col shrink-0 h-min justify-center min-h-[auto] min-w-[auto] order-1 outline-[3px] gap-y-8 w-full pt-0 pb-[22px] px-[15px] md:order-none md:pt-[74px] md:px-8">
+    <main className="relative content-center items-center box-border caret-transparent gap-x-8 flex flex-col shrink-0 h-min justify-center min-h-[auto] min-w-[auto] outline-[3px] gap-y-8 w-full pt-0 pb-[22px] px-[15px] md:pt-[74px] md:px-8">
       <HeroBackground />
       <HeroContent />
     </main>
