7fcdef035c3eb19a597066a4cf5fb76a49ed398e
Benjamin-Manjolo <benjaminmanjolo39@gmail.com>
2026-06-04 16:14:13 +0200

    fixed hero section

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -6,6 +6,7 @@ export const PageContainer = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Hero />
+      <div className="py-8 md:py-0"></div>
       <WorkSection />
       <Footer />
     </div>
